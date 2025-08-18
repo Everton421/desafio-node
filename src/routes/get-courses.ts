a2a5c7aa32bd10dda05aa4e7f7039229aedec22e
everton421 <evertonjose421@gmail.com>
2025-08-18 17:00:39 -0300

    180820251700

--- src/routes/get-courses.ts
+++ src/routes/get-courses.ts
@@ -2,6 +2,7 @@ import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import { db } from '../database/client.ts'
 import { courses } from '../database/schema.ts'
 import z from 'zod'
+import { asc, ilike } from 'drizzle-orm'
 
 export const  getCousesRoute :FastifyPluginAsyncZod = async (server ) => {
     server.get('/courses', 
@@ -9,6 +10,10 @@ export const  getCousesRoute :FastifyPluginAsyncZod = async (server ) => {
             schema:{
                 tags:['courses'] ,
                 summary:"Get all courses",
+                querystring: z.object({
+                    search: z.string().optional(),
+                    orderBy: z.enum(['title','id']).optional().default('title')
+                }),
                 response:{
                     200: z.object({
                         courses: z.array( 
@@ -22,10 +27,18 @@ export const  getCousesRoute :FastifyPluginAsyncZod = async (server ) => {
             }
         }
         ,async  ( request, reply )=>{
+            
+            const { search, orderBy } = request.query
+            
             const result = await db.select( { 
                 id: courses.id,
                 title: courses.title
-            }).from(courses)
+            })
+            .from(courses)
+            .where( 
+                search ? ilike(courses.title, `%${search}%` ) : undefined
+            )
+            .orderBy(asc(courses[orderBy]))
             return  reply.send({ courses:result  }) 
     })
 }
