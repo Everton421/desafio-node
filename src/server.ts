3111f7487bbe80464296dff129e8782330d1b908
Everton421 <evertonjose421@gmail.com>
2025-08-30 12:37:08 -0300

    300820251215(image)

--- src/server.ts
+++ src/server.ts
@@ -3,4 +3,4 @@ import { server } from "./app.ts"
  
 const port = 3000 
  
-server.listen({ port:port, }).then(()=> console.log(`Servidor rodando porta : ${port}`))
+server.listen({ port:port,host: '0.0.0.0' }).then(()=> console.log(`Servidor rodando porta : ${port}`))
