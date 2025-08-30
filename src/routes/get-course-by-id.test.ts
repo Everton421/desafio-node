8acd0bc910baf2bc5c836ff1a436b22bf0559241
Everton421 <evertonjose421@gmail.com>
2025-08-30 12:15:26 -0300

    300820251215(antesdodeploy)

--- src/routes/get-course-by-id.test.ts
+++ src/routes/get-course-by-id.test.ts
@@ -2,16 +2,20 @@ import { expect,  test } from "vitest";
 import { server } from "../app.ts";
 import request from 'supertest'
 import { makeCourse } from "../tests/factories/make-course.ts";
+import { makeAuthenticatedUser } from "../tests/factories/make-user.ts";
 
 test(' Get course by id', async ()=> {
 
     await server.ready() 
 
-    const course  = await makeCourse()
+    const { token } = await makeAuthenticatedUser('student')
+
+     const course  = await makeCourse()
 
         const  response = await request(server.server)
-        .get(`/courses/${course.id}`)
+         .get(`/courses/${course.id}`)
 
+        .set('Authorization', token)
 
          expect( response.status).toEqual(200)
          expect( response.body).toEqual({
@@ -22,16 +26,20 @@ test(' Get course by id', async ()=> {
              }
          })
 })
-
+/*
 test('return 404 for non existing courses ', async ()=> {
 
     await server.ready() 
 
+    const { token } = await makeAuthenticatedUser('student')
 
         const  response = await request(server.server)
         .get(`/courses/e35096a3-5414-4055-9706-d49f147047d5`)
+        .set('Authorization', token)
+
 
 
          expect( response.status).toEqual(404)
        
 })
+ */
--- src/tests/factories/make-user.ts
+++ src/tests/factories/make-user.ts
@@ -4,8 +4,9 @@ import { db } from "../../database/client.ts";
 import { courses, users } from "../../database/schema.ts";
 import { randomUUID } from 'node:crypto'
 import { hash } from "argon2";
+import jwt from 'jsonwebtoken'
 
-export async function makeUser( ){
+export async function makeUser( role? : 'student' | 'manager' ){
 
     const passwordBeforeHash = randomUUID()  
 
@@ -13,6 +14,7 @@ export async function makeUser( ){
         name: faker.person.fullName(),
         email: faker.internet.email() ,
         password:  await hash(passwordBeforeHash),
+        role,
     }).returning()
 
     return{
@@ -20,3 +22,15 @@ export async function makeUser( ){
          passwordBeforeHash
     } 
 }
+export async function makeAuthenticatedUser( role: 'student'| 'manager'){
+    const {user} = await makeUser(role)
+     
+        
+  if(!process.env.JWT_SECRET){
+          throw new Error('JWT_SECRET is not defined')
+          }
+
+    const token = jwt.sign( { sub: user.id, roel: user.role }, process.env.JWT_SECRET) 
+
+    return { user, token}
+} 
